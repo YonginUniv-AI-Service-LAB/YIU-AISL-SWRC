4d541b347257ba1e165c78d2323c56f351ea475f
asempep <who1333@gmail.com>
2025-04-12 00:44:06 +0900

    admin header

--- src/components/details/admin/AdminHeader.vue
+++ src/components/details/admin/AdminHeader.vue
@@ -1,7 +1,7 @@
 <!-- Admin_header.vue -->
 <template>
-  <header class="bg-gray-800 text-white p-4">
-    <h1 class="text-2xl">스포츠 웰니스 관리자</h1>
+  <header class="bg-gray-800 text-white p-12">
+    <h1 class="text-6xl font-bold pl-12 my-8">스포츠 웰니스 관리자</h1>
   </header>
 </template>
 
@@ -16,5 +16,10 @@ export default {
 header {
   background-color: #2d3748; /* 다크 그레이 */
   color: white;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
